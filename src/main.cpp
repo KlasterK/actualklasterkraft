03a318f73d28706654f26c5c9abbfe1246110732
KlasterK <kadakinvi2@gmail.com>
2026-08-03 20:57:13 +0900

    Add support of building on MSYS2
    If WIN32 then winsock and winsock2 are automatically linked.
    
    If MSYS2 stdlibc++ somewhy lacks std::__write_to_terminal and std::__open_terminal functions (like in my case) then you can enable a variable in CMake cache and a custom implementation will be enabled.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
+#ifdef ACTUALKLASTERKRAFT_IMPLEMENT_STD_PRINT_TERMINAL_FUNCTIONS_WIN32
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h>
+#endif
+
 #include <boost/asio.hpp>
+#include <cstdio>
 #include <print>
+#include <system_error>
 
 import actualklasterkraft.acceptor;
 
@@ -14,3 +21,20 @@ int main()
 
     return 0;
 }
+
+#ifdef ACTUALKLASTERKRAFT_IMPLEMENT_STD_PRINT_TERMINAL_FUNCTIONS_WIN32
+    namespace std
+    {
+        void *__open_terminal(FILE *)
+        {
+            return GetStdHandle(STD_OUTPUT_HANDLE);
+        }
+
+        error_code __write_to_terminal(void *handle, span<char> str)
+        {
+            if(!WriteFile(handle, str.data(), str.size(), nullptr, nullptr))
+                return std::error_code(GetLastError(), std::generic_category());
+            return {};
+        }
+    }
+#endif
